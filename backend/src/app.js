a17ba808e1a0684a2abc32b1ab94b00da314bee8
Adoni1234 <adonibrito999@gmail.com>
2025-12-11 13:19:25 -0400

    fixed frotend x backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,18 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+// Array en memoria para los usuarios
+const users = [];
 
-app.use(express.json());
-
-// Servir frontend estático
-app.use(express.static(path.join(__dirname, "../public")));
-
-// Rutas API
+// GET existente
 app.get("/api/users", (req, res) => {
-  res.json([{ name: "Luis" }]);
+  res.status(200).json(users);
 });
 
-// Catch-all para SPA o páginas normales
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+// POST para agregar usuario
+app.post("/api/users", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+  const newUser = { name };
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
-
-module.exports = app;
